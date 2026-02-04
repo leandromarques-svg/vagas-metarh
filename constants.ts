508b34dce2474ff638b27d5a8252505e9a001a95
Leandro Marques <leandromarques@grupometarh.local>
2026-02-04 12:50:29 -0300

    nova senha para api

--- constants.ts
+++ constants.ts
@@ -3,6 +3,7 @@
 
 export const SELECTY_APP_ID = "42c1bbaf-f76c-42a5-b1ca-54fa66c8488b";
 export const SELECTY_SECRET = "8DvpZ9MoEt5rB4i";
+export const SELECTY_API_TOKEN = "eyJpdiI6IjRjZkQvTGlGcFlGNEZza0NlNlFkOWc9PSIsInZhbHVlIjoiSGp0S3lqVkJSVVRPUFY3ZkcreWV1bit3Qklua0Z0enlkb3ErblNwVXRrST0iLCJtYWMiOiI5MjIxMTBmNWY2ZmYyZmE4NjcyNjFmZjkwOTU1OTk4MjJhNGE3Y2JjYzk2YjJmNDdmOGEyZjEwMmExMGY4MzlhIiwidGFnIjoiIn0=";
 
 // API Base URL
 export const API_BASE_URL = "https://api.selecty.app/v2";
